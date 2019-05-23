c14e76df5ee3106cfc7cdb6a5d859ef4eb0a50d2
Bogost <bogumilostapiuk@gmail.com>
2019-05-23 14:36:56 +0200

    build: Utworzenie klasy RequestItemBuilder

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/RequestItemBuilder.java
@@ -0,0 +1,30 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductDataBuilder;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class RequestItemBuilder {
+
+    ProductData productData = new ProductDataBuilder().build();
+    int quantity = 1;
+    Money totalCost = new Money(1);
+
+    RequestItemBuilder() {}
+
+    RequestItemBuilder withProductData(ProductData pd) {
+        productData = pd;
+        return this;
+    }
+
+    RequestItemBuilder withQuantity(int i) {
+        quantity = i;
+        return this;
+    }
+
+    RequestItemBuilder withTotalCost(Money m) {
+        totalCost = m;
+        return this;
+    }
+
+}
